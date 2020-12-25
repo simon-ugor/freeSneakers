a981e16a32a94cd05119c838bbef4e84326e4f84
simon-ugor <simiugor@gmail.com>
2020-12-25 01:58:28 +0100

    some clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 const Header = () => {
     return (
         <div className="main-header-div">
-            <h2 className="heading">FreeSneakers.io 0.1</h2>
+            <h2 className="heading">FreeSneakers.io</h2>
         </div>
     )
 }
