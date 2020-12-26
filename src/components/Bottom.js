375b5e66ac5df752d33aa85a26302e1fe835c9f9
simon-ugor <simiugor@gmail.com>
2020-12-27 00:42:03 +0100

    testing using href

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -6,14 +6,34 @@ const Bottom = () => {
     return (
         <div className="bottom-main-div">
             <div className="bottom-div">
-                <Link className="link" to="/blog">Blog</Link>
-                <Link className="link" to="/">Homepage</Link>
-                <Link className="link" to="/aboutus">About Us</Link>
+
+                <div className="bottom-div1">
+                    <a className="link" href="/blog">Blog</a>
+                </div>
+
+                <div className="bottom-div2">
+                    <a className="link" href="/">Homepage</a>
+                </div>
+
+                <div className="bottom-div3">
+                    <a className="link" href="/aboutus">About Us</a>
+                </div>
+
             </div>
-            <div className="bottom-div2">
-                <Link className="link" to="/privacypolicy">Privacy Policy</Link>
-                <Link className="link" to="/termsandconditions">Terms and Conditions</Link>
-                <Link className="link" to="/contactus">Contact Us</Link>
+            <div className="bottom-div02">
+                
+                <div className="bottom-div1">
+                    <Link className="link" to="/privacypolicy">Privacy Policy</Link>
+                </div>
+
+                <div className="bottom-div2">
+                    <Link className="link" to="/termsandconditions">Terms and Conditions</Link>
+                </div>
+                
+                <div className="bottom-div3">
+                    <Link className="link" to="/contactus">Contact Us</Link>
+                </div>
+                
             </div>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 const Header = () => {
     return (
         <div className="main-header-div">
-            <h2 className="heading">FreeSneakers.xyz</h2>
+            <h2 className="heading">FreeSneakers.xyz02</h2>
         </div>
     )
 }
